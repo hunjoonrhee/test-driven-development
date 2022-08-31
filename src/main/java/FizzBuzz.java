93cb34e0d67e876ddc38268885df91303d4efb02
hunjoonrhee <hunjoonrhee90@gmail.com>
2022-08-31 16:24:13 +0200

    Modified fizzBuzz (add if- else if)

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -3,7 +3,14 @@ public class FizzBuzz {
     public static String fizzBuzz(int value){
         // TODO - implement
 
-
-        return "fizz";
+        if(value == 3){
+            return "fizz";
+        } else if (value == 5) {
+            return "buzz";
+        } else if (value == 15) {
+            return "fizzbuzz";
+        }else {
+            return String.valueOf(value);
+        }
     }
 }
